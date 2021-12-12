ac61af54bd998f995394d4394ec196533d0d535d
thidinhxm <thidinhxm@gmail.com>
2021-12-12 23:50:37 +0700

    setup views

--- src/Admin/app.js
+++ src/Admin/app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const hbs = require('hbs');
+const exphbs = require('express-handlebars');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', exphbs.engine({
+	extname: 'hbs',
+	defaultLayout: 'layout',
+}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -19,25 +23,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- src/StudentTutor/app.js
+++ src/StudentTutor/app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const exphbs = require('express-handlebars');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -10,6 +11,11 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', exphbs.engine({
+	extname: 'hbs',
+	defaultLayout: 'layout',
+}));
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -22,18 +28,18 @@ app.use('/', indexRouter);
 // app.use('/tutor', tutorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
