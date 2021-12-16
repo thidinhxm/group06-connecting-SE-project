d7a003e9724160dd70dff34bc31623dfcd6afdd9
thidinhxm <thidinhxm@gmail.com>
2021-12-16 22:46:24 +0700

    add models and fix layout

---
+++ src/StudentTutor/models/account.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('account', {
+    account_id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER.UNSIGNED.ZEROFILL,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.CHAR(60),
+      allowNull: false
+    },
+    is_locked: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0
+    }
+  }, {
+    sequelize,
+    tableName: 'account',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "account_id" },
+        ]
+      },
+    ]
+  });
+};
