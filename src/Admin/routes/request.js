467a39754c2815333992e3c21c6b6b835756d241
alibaba-boop-byte <trinhchau.1006@gmail.com>
2021-12-16 20:53:14 +0700

    commit

--- src/Admin/routes/request.js
+++ src/Admin/routes/request.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const requestController = require('../controllers/request');
+const requestController = require("../controllers/request");
 
-router.get('/tutor-requests', requestController.listTutorRequests);
-router.get('/tutor-request/:id', requestController.showTutorRequest);
+router.get("/tutor-requests", requestController.listTutorRequests);
+router.get("/tutor-requests/:id", requestController.showTutorRequest);
 
-router.get('/student-requests', requestController.listStudentRequests);
-router.get('/student-request/:id', requestController.showStudentRequest);
+router.get("/student-requests", requestController.listStudentRequests);
+router.get("/student-requests/:id", requestController.showStudentRequest);
 
 module.exports = router;
