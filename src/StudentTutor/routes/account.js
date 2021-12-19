93d9aa224aa6e08db3f7a7d8afc6d6348286c36c
CalmNguyen <92631325+CalmNguyen@users.noreply.github.com>
2021-12-19 10:22:54 +0700

    Update account.js

--- src/StudentTutor/routes/account.js
+++ src/StudentTutor/routes/account.js
@@ -6,5 +6,6 @@ const accountController = require('../controllers/account');
 router.get('/profile', accountController.profile);
 router.get('/login', accountController.login);
 router.get('/signup', accountController.signup);
+router.get('/signupTutor', accountController.signupTutor);
 
 module.exports = router;
