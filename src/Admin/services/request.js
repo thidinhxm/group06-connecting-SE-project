959f4ddfb9999238e7feac81be2e41c1da7be18e
thidinhxm <thidinhxm@gmail.com>
2021-12-25 00:18:06 +0700

    fix view request feature

--- src/Admin/services/request.js
+++ src/Admin/services/request.js
@@ -8,10 +8,6 @@ exports.listTutorRequests = async () => {
 
 exports.showTutorRequest = async (id) => {
     return await models.tutorrequest.findOne({
-        include : [{
-            model: models.request,
-            as: 'tutor_request',
-        }],
         where: {
             tutor_request_id: id
         },
@@ -27,22 +23,9 @@ exports.listStudentRequests = async () => {
 
 exports.showStudentRequest = async (id) => {
     return await models.studentrequest.findOne({
-        include: [{
-                model: models.request,
-                as: 'student_request',
-            }, {
-                model: models.gradesubject,
-                as: 'student_request',
-            }, 
-            {
-                model: models.subject,
-                as: 'subject',
-            }, {
-                model: models.grade,
-                as: 'grade',
-            }],
         where: {
             student_request_id: id
-        }
+        },
+        raw: true
     });
 }
