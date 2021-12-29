ecbf6c94d676d0fce3ff679508cd0784bac74058
CalmNguyen <92631325+CalmNguyen@users.noreply.github.com>
2021-12-29 13:14:18 +0700

    Get data for profile and change pw

--- src/StudentTutor/controllers/account.js
+++ src/StudentTutor/controllers/account.js
@@ -2,10 +2,6 @@ const bcrypt = require('bcrypt');
 
 const accountService = require('../services/account');
 
-exports.profile = (req, res, next) => {
-    res.render('account/profile');
-}
-
 exports.login = (req, res, next) => {
     res.render('account/login');
 }
@@ -51,4 +47,35 @@ exports.forgotPassword = async(req, res, next) => {
 exports.logout = async(req, res, net) => {
     res.logout();
     res.redirect('/');
+}
+
+exports.profile = async(req, res, next) => { 
+    var id_user = req.params.account_id;
+    const profile = await accountService.getInforProfileByID(id_user);
+    const profile_acc = await accountService.getAccForProfile(id_user);
+    console.log("Đây là thông tin người dùng");
+    console.log(profile);
+    console.log(profile_acc);
+    res.render('account/profile', {profile, profile_acc});
+}
+
+exports.changePassword = async(req, res, next) => { 
+    var new_pw = req.body.new_password;
+    const len = new_pw.length;
+    const hashPassword = bcrypt.hashSync(new_pw, len);
+    var present_pw = req.body.present_password;
+    var email = req.body.email_for_cp;
+    const pw = await accountService.getPassword(email);
+    var account_id=pw.account_id;
+    if(bcrypt.compareSync(present_pw, pw.password))
+    {
+        console.log("Mật khẩu đã được cập nhật.");
+        await accountService.updatePassword(email, hashPassword);
+        res.redirect('/profile/'+ account_id);
+        console.log("Mật khẩu đã được cập nhật.");
+    }
+    else{
+        res.redirect('/profile/'+ account_id);
+        console.log("Sai mật khẩu vui lòng nhập lại.");
+    }
 }
