c99929bf354a23da3c0a4dec1f46feced969cd43
CalmNguyen <92631325+CalmNguyen@users.noreply.github.com>
2021-12-29 11:13:05 +0700

    Add function change password admin

--- src/Admin/services/account.js
+++ src/Admin/services/account.js
@@ -15,4 +15,36 @@ exports.getAdminAccountByEmail = async (email) => {
     });
     return admin;
 }
+exports.getInforProfileByEmail = async() => {
+    return await models.admin.findOne({ 
+            raw: true,
+    });
+}
+exports.getAccForProfile = async(id_admin) =>{
+    return await models.account.findOne({
+        where:{
+            account_id: id_admin,
+        },
+        raw:true,
+    }
+    );
+}
 
+exports.updatePassword = async(email_, new_pw) =>{
+    return models.account.update({
+        password: new_pw,
+    }, {
+        where: {
+            email: email_,
+        },
+});
+}
+
+exports.getPassword = async(email)=>{
+    return await models.account.findOne({
+        where:{
+            email:email,
+        },
+        raw:true,
+    });
+}
