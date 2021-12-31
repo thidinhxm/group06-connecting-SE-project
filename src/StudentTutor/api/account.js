189d2e34927b41d76a036357b97fb4fab3437e91
thidinhxm <thidinhxm@gmail.com>
2021-12-31 13:02:35 +0700

    add verify account when register

--- src/StudentTutor/api/account.js
+++ src/StudentTutor/api/account.js
@@ -1,20 +1,4 @@
 const accountService = require('../services/account');
-const bcrypt = require('bcrypt');
-exports.checkAccount = async (req, res, next) => {
-    try {
-        const {email, password} = req.body;
-        const account = await accountService.getUserByEmail(email, password);
-        if (account && bcrypt.compareSync(password, account.password)) {
-            res.json(true);
-        }
-        else {
-            res.json(false);
-        }
-    }
-    catch (err) {
-        next(err);
-    }
-}
 
 exports.checkExistAccount = async (req, res, next) => {
     try {
