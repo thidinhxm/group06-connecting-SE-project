05c7af59f1a1a5ae1b12d69c60b867c5791aff3e
QuanNguyen0804 <92315466+QuanNguyen0804@users.noreply.github.com>
2022-01-05 16:47:57 +0700

    update myRequest

---
+++ src/StudentTutor/controllers/request.js
@@ -0,0 +1,48 @@
+const { models } = require('../models');
+const requestService = require('../services/request');
+
+exports.RequestList = async (req, res, next) => {
+    try {
+        let requests, myRequests;
+        
+        if (req.user.student_id) {
+            requests = await requestService.listStudentRequests();
+
+            myRequests = requests.filter((request) => {
+                return request.student_id == req.user.student_id;
+            })
+
+            return res.render('students/myRequests/myRequestList', {studentRequests: myRequests});
+        } else if (req.user.tutor_id) {
+            requests = await requestService.listTutorRequests();
+
+            myRequests = requests.filter((request) => {
+                return request.tutor_id == req.user.tutor_id;
+            })
+
+            return res.render('tutor/myRequests/myRequestList', {tutorRequests: myRequests});
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+exports.RequestDetail = async (req, res, next) => {
+    try {
+        let requestDetail;
+
+        if (req.user.student_id) {
+            requestDetail = await requestService.showStudentRequest(req.params.id);
+
+            return res.render('students/myRequests/myRequestDetail', {studentRequest: requestDetail})
+        } else if (req.user.tutor_id) {
+            requestDetail = await requestService.showStudentRequest(req.params.id);
+
+            return res.render('tutor/myRequests/myRequestDetail', {tutorRequest: requestDetail})
+        }
+        
+    } catch (error) {
+        next(error);
+    }
+}
---
+++ src/StudentTutor/services/request.js
@@ -0,0 +1,32 @@
+const { models } = require('../models');
+
+exports.listTutorRequests = async () => {
+	return await models.tutorrequest.findAll({
+		raw: true,
+	});
+};
+
+exports.showTutorRequest = async (id) => {
+	return await models.tutorrequest.findOne({
+		where: {
+			tutor_request_id: id,
+		},
+		raw: true,
+	});
+};
+
+exports.listStudentRequests = async () => {
+	return await models.studentrequest.findAll({
+		raw: true,
+	});
+};
+
+exports.showStudentRequest = async (id) => {
+	return await models.studentrequest.findOne({
+		where: {
+			student_request_id: id,
+		},
+		raw: true,
+	});
+};
+
