66341139088ae0e8c16a3de319298ec9569fe9fb
alibaba-boop-byte <trinhchau.1006@gmail.com>
2022-01-05 14:00:02 +0700

    search-function

--- src/Admin/controllers/user.js
+++ src/Admin/controllers/user.js
@@ -1,73 +1,65 @@
-const userService = require('../services/user');
+const userService = require("../services/user");
 
 exports.listTutors = async (req, res, next) => {
-    try {
-        const tutors = await userService.listTutors();
-        res.render('users/tutors/tutorList', { tutors });
-    }
-    catch (err) {
-        next(err);
-    }
-}
+  try {
+    const keyword = req.query.query || "";
+    const tutors = await userService.listTutors(keyword);
+    res.render("users/tutors/tutorList", { tutors });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.showTutor = async (req, res, next) => {
-    try {
-        const tutor = await userService.showTutor(req.params.id);
-        res.render('users/tutors/tutorDetail', { tutor });
-    }
-    catch (err) {
-        next(err);
-    }
-}
+  try {
+    const tutor = await userService.showTutor(req.params.id);
+    res.render("users/tutors/tutorDetail", { tutor });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.listStudents = async (req, res, next) => {
-    try {
-        const students = await userService.listStudents();
-        res.render('users/students/studentList', { students });
-    }
-    catch (err) {
-        next(err);
-    }
-}
-
+  try {
+    const keyword = req.query.query || "";
+    const students = await userService.listStudents(keyword);
+    res.render("users/students/studentList", { students });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.showStudent = async (req, res, next) => {
-    try {
-        const student = await userService.showStudent(req.params.id);
-        res.render('users/students/studentDetail', { student });
-    }
-    catch (err) {
-        next(err);
-    }   
-}
+  try {
+    const student = await userService.showStudent(req.params.id);
+    res.render("users/students/studentDetail", { student });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.lock = async (req, res, next) => {
-    try {
-        await userService.lock(req.params.id);
-        if (req.body.userType == 'tutor') {
-            res.redirect('/users/tutors');
-        }
-        else {
-            res.redirect('/users/students');
-        }
+  try {
+    await userService.lock(req.params.id);
+    if (req.body.userType == "tutor") {
+      res.redirect("/users/tutors");
+    } else {
+      res.redirect("/users/students");
     }
-    catch (err) {
-        next(err);
-    }
-}
-
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.unlock = async (req, res, next) => {
-    try {
-        await userService.unlock(req.params.id);
-        if (req.body.userType == 'tutor') {
-            (res.redirect('/users/tutors'));
-        }
-        else {
-            (res.redirect('/users/students'));
-        }
+  try {
+    await userService.unlock(req.params.id);
+    if (req.body.userType == "tutor") {
+      res.redirect("/users/tutors");
+    } else {
+      res.redirect("/users/students");
     }
-    catch (err) {
-        next(err);
-    }
-}
+  } catch (err) {
+    next(err);
+  }
+};
