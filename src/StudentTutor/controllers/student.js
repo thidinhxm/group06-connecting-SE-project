8a3678b1b7e9a4b43b213cc160d58aaf3f44038d
QuanNguyen0804 <92315466+QuanNguyen0804@users.noreply.github.com>
2022-01-08 20:45:00 +0700

    fix view

--- src/StudentTutor/controllers/student.js
+++ src/StudentTutor/controllers/student.js
@@ -11,6 +11,7 @@ exports.sendRequest = async (req, res, next) => {
 };
 
 exports.storeRequest = async (req, res, next) => {
+<<<<<<< Updated upstream
   const classroom = 'Lớp ' + req.body.classroom.toString()
   const subjects = req.body.subject.toString()
   const homeNumber = req.body.homeNumber;
@@ -42,6 +43,39 @@ exports.storeRequest = async (req, res, next) => {
   await models.studentrequest.create(newStudentRequest)
   res.json(newStudentRequest);
   // console.log(subjects.toString());
+=======
+	const classroom = 'Lớp ' + req.body.classroom.toString()
+	const subjects = req.body.subject.toString()
+	const homeNumber = req.body.homeNumber;
+	const ward = req.body.ward
+	const road = req.body.road
+	const city = req.body.city
+	const address = homeNumber + ',' + road + ',' + ward + ',' + city
+	const salary = req.body.fee
+	const phone = req.body.phone
+	const time = req.body.time
+	const other_request = req.body.different
+
+	const studentID = req.user.account_id;// maybe change when login complete
+
+
+	const newStudentRequest = {
+		student_id: studentID,
+		address: address,
+		phone: phone,
+		salary: salary,
+		grade: classroom,
+		subject: subjects,
+		time: time,
+		other_request: other_request,
+	};
+
+
+
+	await models.studentrequest.create(newStudentRequest)
+	res.redirect('/my-request');
+	// console.log(subjects.toString());
+>>>>>>> Stashed changes
 };
 
 
